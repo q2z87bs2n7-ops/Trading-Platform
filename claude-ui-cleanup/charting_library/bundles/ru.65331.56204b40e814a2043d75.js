f207d3e6187d68f3e3a14058453c53ca3328b7c8
q2z87bs2n7-ops <q2z87bs2n7-ops@users.noreply.github.com>
2026-05-19 15:43:04 +0000

    deploy: 3ea0ae1290807d9db8e2c565c5a55d71746ed5f3

---
+++ claude-ui-cleanup/charting_library/bundles/ru.65331.56204b40e814a2043d75.js
@@ -0,0 +1,1 @@
+(self.webpackChunktradingview=self.webpackChunktradingview||[]).push([[65331],{202778:e=>{e.exports={en:["Collapse panel"],ru:["Свернуть панель"]}},923882:e=>{e.exports={en:["Open panel"],ru:["Открыть панель"]}},597828:e=>{e.exports={en:["Maximize panel"],ru:["Развернуть панель"]}},904078:e=>{e.exports={en:["Restore panel"],ru:["Восстановить панель"]}}}]);
