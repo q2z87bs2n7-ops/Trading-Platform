945bc5126d7de5f3fd9c54b8f64d7c58edfe9b18
q2z87bs2n7-ops <q2z87bs2n7-ops@users.noreply.github.com>
2026-05-19 20:37:38 +0000

    deploy: 5788f896b75afd243c93c3b7cebbf27003f2821a

---
+++ claude-review-failed-commits-bDkOb/charting_library/bundles/tr.22510.97ea9d1230f787704bab.js
@@ -0,0 +1,1 @@
+(self.webpackChunktradingview=self.webpackChunktradingview||[]).push([[22510],{277011:e=>{e.exports={en:["A"],tr:["O"]}},420186:e=>{e.exports={en:["L"]}},372719:e=>{e.exports={en:["Toggle auto scale"],tr:["Otomatik ölçeği aç / kapat"]}},206451:e=>{e.exports={en:["Toggle log scale"],tr:["Log ölçeğini değiştir"]}}}]);
