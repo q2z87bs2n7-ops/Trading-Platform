86dd327c83e3ddd54f1522bb99a04991c37bdb9f
q2z87bs2n7-ops <q2z87bs2n7-ops@users.noreply.github.com>
2026-05-20 17:59:27 +0000

    deploy: a9a270b0461944069a7528b221a47e2766560b69

---
+++ claude-watchlist-search-ui-polish/charting_library/bundles/id_ID.85577.a6087f2550d8a552986a.js
@@ -0,0 +1,1 @@
+(self.webpackChunktradingview=self.webpackChunktradingview||[]).push([[85577],{99529:e=>{e.exports={en:["Add to favorites"],id_ID:["Tambah ke daftar favorit"]}},694209:e=>{e.exports={en:["Remove from favorites"],id_ID:["Hapus dari favorit"]}}}]);
