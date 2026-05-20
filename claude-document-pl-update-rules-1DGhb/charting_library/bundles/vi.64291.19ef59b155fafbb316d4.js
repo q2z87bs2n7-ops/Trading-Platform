99a594fc1b1ac16272d9e759e46eec129d6c6509
q2z87bs2n7-ops <q2z87bs2n7-ops@users.noreply.github.com>
2026-05-20 14:13:45 +0000

    deploy: 0aa89055a29fe942109c83ed92dcb5b5f729240a

---
+++ claude-document-pl-update-rules-1DGhb/charting_library/bundles/vi.64291.19ef59b155fafbb316d4.js
@@ -0,0 +1,1 @@
+(self.webpackChunktradingview=self.webpackChunktradingview||[]).push([[64291],{338554:e=>{e.exports={en:["Cancel order"],vi:["Hủy lệnh"]}},59855:e=>{e.exports={en:["Keep order"],vi:["Giữ lệnh"]}},93318:e=>{e.exports={en:["Cancel order"],vi:["Hủy lệnh"]}}}]);
