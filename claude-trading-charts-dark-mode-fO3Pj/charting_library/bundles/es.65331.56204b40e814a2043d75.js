770aa25480d4b0900a01f9706a75ec2e923fa7bb
q2z87bs2n7-ops <q2z87bs2n7-ops@users.noreply.github.com>
2026-05-24 01:59:14 +0000

    deploy: 93759b101ec584b9363c2edd6cdfdc2840285adc

---
+++ claude-trading-charts-dark-mode-fO3Pj/charting_library/bundles/es.65331.56204b40e814a2043d75.js
@@ -0,0 +1,1 @@
+(self.webpackChunktradingview=self.webpackChunktradingview||[]).push([[65331],{202778:e=>{e.exports={en:["Collapse panel"],es:["Colapsar panel"]}},923882:e=>{e.exports={en:["Open panel"],es:["Abrir panel"]}},597828:e=>{e.exports={en:["Maximize panel"],es:["Maximizar el panel"]}},904078:e=>{e.exports={en:["Restore panel"],es:["Restablecer panel"]}}}]);
---
+++ claude-trading-charts-dark-mode-fO3Pj/charting_library/bundles/th.85577.a6087f2550d8a552986a.js
@@ -0,0 +1,1 @@
+(self.webpackChunktradingview=self.webpackChunktradingview||[]).push([[85577],{99529:e=>{e.exports={en:["Add to favorites"],th:["เพิ่มในรายการโปรด"]}},694209:e=>{e.exports={en:["Remove from favorites"],th:["ลบออกจากรายการโปรด"]}}}]);
