73c9350a8ca54aada49af52a07442801e868d8c7
q2z87bs2n7-ops <q2z87bs2n7-ops@users.noreply.github.com>
2026-05-25 05:35:01 +0000

    deploy: 6af74e85d9624b572a6399c0148bcda71062bdf9

---
+++ claude-app-features-review-Gfo3Q/charting_library/bundles/zh_TW.26960.7868abe2e7ab4d0739e5.js
@@ -0,0 +1,1 @@
+(self.webpackChunktradingview=self.webpackChunktradingview||[]).push([[26960],{43812:e=>{e.exports={en:["Got it!"],zh_TW:["了解！"]}},402324:e=>{e.exports={en:["OK"],zh_TW:["確認"]}},204496:e=>{e.exports={en:["Press and hold to see detailed chart values"],zh_TW:["按住可查看詳細圖表值"]}}}]);
