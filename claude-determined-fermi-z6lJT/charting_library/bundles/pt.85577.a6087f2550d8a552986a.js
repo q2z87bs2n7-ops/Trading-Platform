f6d89aa64d851a25778bb4fa2ba18cdf8b97ffbd
q2z87bs2n7-ops <q2z87bs2n7-ops@users.noreply.github.com>
2026-05-25 14:17:53 +0000

    deploy: ef029091b7c2b324a155abf0446c2e4b0373608f

---
+++ claude-determined-fermi-z6lJT/charting_library/bundles/pt.85577.a6087f2550d8a552986a.js
@@ -0,0 +1,1 @@
+(self.webpackChunktradingview=self.webpackChunktradingview||[]).push([[85577],{99529:e=>{e.exports={en:["Add to favorites"],pt:["Adicionar aos favoritos"]}},694209:e=>{e.exports={en:["Remove from favorites"],pt:["Remover dos favoritos"]}}}]);
