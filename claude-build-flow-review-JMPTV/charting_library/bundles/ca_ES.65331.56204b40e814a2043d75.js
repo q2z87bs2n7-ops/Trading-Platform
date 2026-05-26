32826460f1fee73f6f8af8339efd461d6b361b47
q2z87bs2n7-ops <q2z87bs2n7-ops@users.noreply.github.com>
2026-05-26 00:14:17 +0000

    deploy: b437794372aab8039dad9c85a5f5202287ac156f

---
+++ claude-build-flow-review-JMPTV/charting_library/bundles/ca_ES.65331.56204b40e814a2043d75.js
@@ -0,0 +1,1 @@
+(self.webpackChunktradingview=self.webpackChunktradingview||[]).push([[65331],{202778:e=>{e.exports={en:["Collapse panel"]}},923882:e=>{e.exports={en:["Open panel"]}},597828:e=>{e.exports={en:["Maximize panel"]}},904078:e=>{e.exports={en:["Restore panel"]}}}]);
---
+++ claude-build-flow-review-JMPTV/charting_library/bundles/ms_MY.65331.56204b40e814a2043d75.js
@@ -0,0 +1,1 @@
+(self.webpackChunktradingview=self.webpackChunktradingview||[]).push([[65331],{202778:e=>{e.exports={en:["Collapse panel"],ms_MY:["Runtuhkan panel"]}},923882:e=>{e.exports={en:["Open panel"],ms_MY:["Buka panel"]}},597828:e=>{e.exports={en:["Maximize panel"],ms_MY:["Maksimumkan panel"]}},904078:e=>{e.exports={en:["Restore panel"],ms_MY:["Kembalikan panel"]}}}]);
