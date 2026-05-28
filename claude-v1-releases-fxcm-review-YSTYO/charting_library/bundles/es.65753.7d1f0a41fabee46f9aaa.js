9a6d9126514fc4f6b76714563351399c3981e1df
q2z87bs2n7-ops <q2z87bs2n7-ops@users.noreply.github.com>
2026-05-28 11:50:54 +0000

    deploy: 181df870ea7dee1fdfc0dcb84d88b2270ea5678d

---
+++ claude-v1-releases-fxcm-review-YSTYO/charting_library/bundles/es.65753.7d1f0a41fabee46f9aaa.js
@@ -0,0 +1,1 @@
+(self.webpackChunktradingview=self.webpackChunktradingview||[]).push([[65753,58899],{903443:e=>{e.exports={en:["Add text"],es:["Añadir texto"]}},858899:e=>{e.exports={en:["change {title} text"],es:["cambiar texto de {title}"]}}}]);
