122f842a882142ee6e20f847cc378d4ea723c476
q2z87bs2n7-ops <q2z87bs2n7-ops@users.noreply.github.com>
2026-05-28 02:04:27 +0000

    deploy: 044a861a08f56b6354e8abb9be09ad91b897e37a

---
+++ claude-busy-ptolemy-WlZ2T/charting_library/bundles/zh_TW.65753.7d1f0a41fabee46f9aaa.js
@@ -0,0 +1,1 @@
+(self.webpackChunktradingview=self.webpackChunktradingview||[]).push([[65753,58899],{903443:e=>{e.exports={en:["Add text"],zh_TW:["新增文字"]}},858899:e=>{e.exports={en:["change {title} text"],zh_TW:["更改{title}文字"]}}}]);
