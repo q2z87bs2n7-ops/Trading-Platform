24129df2c3d2d6aaf6dc47d51b91293f10c45a47
q2z87bs2n7-ops <q2z87bs2n7-ops@users.noreply.github.com>
2026-05-29 00:57:09 +0000

    deploy: fd932ab8f4ea1dadadf3e07ee2b7f35841048e12

---
+++ claude-cfd-scalping-mode-proposal-bJ0KX/charting_library/bundles/th.64291.19ef59b155fafbb316d4.js
@@ -0,0 +1,1 @@
+(self.webpackChunktradingview=self.webpackChunktradingview||[]).push([[64291],{338554:e=>{e.exports={en:["Cancel order"],th:["ยกเลิกออร์เดอร์"]}},59855:e=>{e.exports={en:["Keep order"],th:["เก็บออร์เดอร์ไว้"]}},93318:e=>{e.exports={en:["Cancel order"],th:["ยกเลิกออร์เดอร์"]}}}]);
